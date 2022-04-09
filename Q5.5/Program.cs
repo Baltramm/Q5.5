4af30e2cb65eaabdf34af970b08422304cb7a742
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-10 00:38:56 +0600

    Задание 5.5.8

--- Q5.5/Program.cs
+++ Q5.5/Program.cs
@@ -1,37 +1,24 @@
 ﻿using System;
 
+
 namespace Q5._5
 {
     class Program
     {
-        static void Main(string[] args) { 
-         Console.WriteLine("Напишите что-то");
-  var str = Console.ReadLine();
-
-        Console.WriteLine("Укажите глубину эха");
-  var deep = int.Parse(Console.ReadLine());
-
-        Echo(str, deep);
-
-        Console.ReadKey();
-}
-        static void Echo(string saidworld, int deep)
+        static void Main(string[] args)
         {
-            var modif = saidworld;
-          
+
+        }
 
 
-            if (modif.Length > 2)
-            {
-                modif = modif.Remove(0, 2);
-            }
-            Console.BackgroundColor = (ConsoleColor)deep;
-            Console.WriteLine("..." + modif);
+        private static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+                return 1;
+            else if (pow == 1)
+                return N;
+            else return N * PowerUp(N,--pow);
 
-            if (deep > 1)
-            {
-                Echo(modif, deep - 1);
-            }
         }
     }
 }
