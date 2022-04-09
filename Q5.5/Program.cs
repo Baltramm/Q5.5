c98e85ca9d169776d0a3447ba0816477fcd238bf
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-10 01:54:15 +0600

    80% программы выполнено

--- Q5.5/Program.cs
+++ Q5.5/Program.cs
@@ -7,13 +7,13 @@ namespace Q5._5
     {
         static void Main(string[] args)
         {
-            
+            Anketa();
         }
-        (string name, string lastname, int age, bool haveapet, int petamount) anketa;
+        
 
-        static (string name,string lastname,int age) Anketa()
+        static (string name,string lastname,int age, bool haveapet, int petamount,byte colors) Anketa()
         {
-            (string name,string lastname, int age) anketa;
+            (string name,string lastname, int age, bool haveapet, int petamount, byte colors) anketa;
             Console.WriteLine("Ваше имя:");
             anketa.name = Console.ReadLine();
             Console.WriteLine("Ваша фамилия:");
@@ -25,12 +25,26 @@ namespace Q5._5
                 Console.WriteLine("Ваш возраст:");
                 age = Console.ReadLine();
             }
-            while (ChekNum(age, out intage));
+            while (CheckNum(age, out intage));
             anketa.age = intage;
+            Console.WriteLine("У вас есть питомец,да или нет?");
+            string havepet = Console.ReadLine();
+            if (havepet=="Да" & havepet == "да")
+                    anketa.haveapet =true;
+            else anketa.haveapet = false;
+            if (anketa.haveapet == true)
+                Console.WriteLine("Сколько у вас питомцев");
+            anketa.petamount = int.Parse( Console.ReadLine());
+            CreateArrPets(anketa.petamount);
+            
+            Console.WriteLine("Введите ваши любимые цвета");
+            anketa.colors = byte.Parse(Console.ReadLine());
+            CreateArrColors(anketa.colors);
             return anketa;
         }
         static bool CheckNum (string number,out int corrnumber)
         {
+            
             if (int.TryParse(number,out int intnum))
             {
                 if (intnum>0)
@@ -47,7 +61,21 @@ namespace Q5._5
         static string[] CreateArrPets(int num)
         {
             var result = new string[num];
-
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Ваш питомец номер{0}:", i);
+                result[i] = Console.ReadLine();
+            }
+            return result;
+        }
+        static string[] CreateArrColors(int num)
+        {
+            var result = new string[num];
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Ваш питомец номер{0}:", i);
+                result[i] = Console.ReadLine();
+            }
             return result;
         }
     }
