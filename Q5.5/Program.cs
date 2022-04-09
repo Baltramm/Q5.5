f26f8a3e315bf59ccb95d4b634bbac83d219acb9
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-10 00:59:42 +0600

    Пол работы

--- Q5.5/Program.cs
+++ Q5.5/Program.cs
@@ -7,18 +7,48 @@ namespace Q5._5
     {
         static void Main(string[] args)
         {
-
+            
         }
+        (string name, string lastname, int age, bool haveapet, int petamount) anketa;
 
-
-        private static int PowerUp(int N, byte pow)
+        static (string name,string lastname,int age) Anketa()
         {
-            if (pow == 0)
-                return 1;
-            else if (pow == 1)
-                return N;
-            else return N * PowerUp(N,--pow);
+            (string name,string lastname, int age) anketa;
+            Console.WriteLine("Ваше имя:");
+            anketa.name = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия:");
+            anketa.lastname = Console.ReadLine();
+            string age;
+            int intage;
+            do
+            {
+                Console.WriteLine("Ваш возраст:");
+                age = Console.ReadLine();
+            }
+            while (ChekNum(age, out intage));
+            anketa.age = intage;
+            return anketa;
+        }
+        static bool CheckNum (string number,out int corrnumber)
+        {
+            if (int.TryParse(number,out int intnum))
+            {
+                if (intnum>0)
+                {
+                    corrnumber = intnum;
+                    return false;
+                }
+                {
+                    corrnumber = 0;
+                    return true;
+                }
+            }
+        }
+        static string[] CreateArrPets(int num)
+        {
+            var result = new string[num];
 
+            return result;
         }
     }
 }
